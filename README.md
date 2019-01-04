c65fcb24017879746882dc8b989c8a09e9555e3a
Remco Perlee <remco.perlee@gmail.com>
2019-01-04 22:16:46 +0100

    Final cleanup (it works!)

---
+++ README.md
@@ -0,0 +1,3 @@
+The base image I use for all my docker images.
+
+Uses Ubuntu 18.04 with a few commonly used tools.
--- rebuild.sh
+++
@@ -1,5 +0,0 @@
-# kubectl delete pod -l app=sickrage --namespace=download
-#
-# kubectl apply -f namespace.yaml
-# kubectl apply -f deployment.yaml --namespace=download
-# kubectl apply -f service.yaml    --namespace=download
